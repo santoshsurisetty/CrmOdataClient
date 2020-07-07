f5142a89e8660fa2eef344c68394db76eaaf3d24
Santosh Surisetty <santoshsurisetty@gmail.com>
2020-07-07 21:40:38 +0100

    Entity Tracking & property tracking with DataServiceCollection in .Net core 3.1

--- CrmOdataClient/FilterEdmEntityModel.cs
+++ CrmOdataClient/FilterEdmEntityModel.cs
@@ -1,6 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.OData;
+using Microsoft.OData.Client;
 using Microsoft.OData.Edm;
 
 namespace CrmOdataClient
@@ -27,13 +37,15 @@ namespace CrmOdataClient
                     {
                         if (EdmTypeSemantics.IsCollection(x.Type))
                         {
-                            var collectionElementType = ExtensionMethods.AsElementType(x.Type.Definition) as IEdmEntityType;
+                            var collectionElementType =
+                                ExtensionMethods.AsElementType(x.Type.Definition) as IEdmEntityType;
                             var name = collectionElementType.Name;
                             return _navigationEntityTypes.ContainsKey(name);
                         }
                         else
                         {
-                            var collectionElementType = ExtensionMethods.AsElementType(x.Type.Definition) as IEdmEntityType;
+                            var collectionElementType =
+                                ExtensionMethods.AsElementType(x.Type.Definition) as IEdmEntityType;
                             var name = collectionElementType.Name;
                             return _navigationEntityTypes.ContainsKey(name);
                         }
@@ -60,5 +72,293 @@ namespace CrmOdataClient
         public string Namespace => _model.Namespace;
         public IEnumerable<IEdmStructuralProperty> DeclaredKey => _model.DeclaredKey;
         public bool HasStream => _model.HasStream;
+
+
     }
+
+    public class HttpClientRequestMessage : DataServiceClientRequestMessage
+    {
+        private readonly HttpRequestMessage requestMessage;
+        private readonly HttpClient client;
+        private readonly HttpClientHandler handler;
+        private readonly MemoryStream messageStream;
+        private readonly Dictionary<string, string> contentHeaderValueCache;
+
+        public HttpClientRequestMessage(string actualMethod)
+            : base(actualMethod)
+        {
+            this.requestMessage = new HttpRequestMessage();
+            this.messageStream = new MemoryStream();
+            this.handler = new HttpClientHandler();
+            this.client = new HttpClient(this.handler, disposeHandler: true);
+            this.contentHeaderValueCache = new Dictionary<string, string>();
+        }
+
+        public override IEnumerable<KeyValuePair<string, string>> Headers
+        {
+            get
+            {
+                if (this.requestMessage.Content != null)
+                {
+                    return HttpHeadersToStringDictionary(this.requestMessage.Headers).Concat(HttpHeadersToStringDictionary(this.requestMessage.Content.Headers));
+                }
+
+                return HttpHeadersToStringDictionary(this.requestMessage.Headers).Concat(this.contentHeaderValueCache);
+            }
+        }
+
+        public override Uri Url
+        {
+            get { return requestMessage.RequestUri; }
+            set { requestMessage.RequestUri = value; }
+        }
+
+        public override string Method
+        {
+            get { return this.requestMessage.Method.ToString(); }
+            set { this.requestMessage.Method = new HttpMethod(value); }
+        }
+
+        public override ICredentials Credentials
+        {
+            get { return this.handler.Credentials; }
+            set { this.handler.Credentials = value; }
+        }
+
+        public override int Timeout
+        {
+            get { return (int)this.client.Timeout.TotalSeconds; }
+            set { this.client.Timeout = new TimeSpan(0, 0, value); }
+        }
+
+        public override int ReadWriteTimeout { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value that indicates whether to send data in segments to the Internet resource. 
+        /// </summary>
+        public override bool SendChunked
+        {
+            get
+            {
+                bool? transferEncodingChunked = this.requestMessage.Headers.TransferEncodingChunked;
+                return transferEncodingChunked.HasValue && transferEncodingChunked.Value;
+            }
+            set { this.requestMessage.Headers.TransferEncodingChunked = value; }
+        }
+
+        public override string GetHeader(string headerName)
+        {
+            //Returns the value of the header with the given name.
+            var values = requestMessage.Headers.GetValues(headerName);
+            return values.FirstOrDefault();
+        }
+
+        public override void SetHeader(string headerName, string headerValue)
+        {
+            requestMessage.Headers.Add(headerName, headerValue);
+            // Sets the value of the header with the given name
+        }
+
+        public override Stream GetStream()
+        {
+            return this.messageStream;
+        }
+
+        /// <summary>
+        /// Abort the current request.
+        /// </summary>
+        public override void Abort()
+        {
+            this.client.CancelPendingRequests();
+        }
+
+        public override IAsyncResult BeginGetRequestStream(AsyncCallback callback, object state)
+        {
+            var taskCompletionSource = new TaskCompletionSource<Stream>();
+            taskCompletionSource.TrySetResult(this.messageStream);
+            return taskCompletionSource.Task.ToApm(callback, state);
+        }
+
+        public override Stream EndGetRequestStream(IAsyncResult asyncResult)
+        {
+            return ((Task<Stream>)asyncResult).Result;
+        }
+
+        public override IAsyncResult BeginGetResponse(AsyncCallback callback, object state)
+        {
+            var send = CreateSendTask();
+            return send.ToApm(callback, state);
+        }
+
+        public override IODataResponseMessage EndGetResponse(IAsyncResult asyncResult)
+        {
+            var result = ((Task<HttpResponseMessage>)asyncResult).Result;
+            return ConvertHttpClientResponse(result);
+        }
+
+        public override IODataResponseMessage GetResponse()
+        {
+            var send = CreateSendTask();
+            send.Wait();
+            return ConvertHttpClientResponse(send.Result);
+        }
+
+        private Task<HttpResponseMessage> CreateSendTask()
+        {
+            // Only set the message content if the stream has been written to, otherwise
+            // HttpClient will complain if it's a GET request.
+            var messageContent = this.messageStream.ToArray();
+            if (messageContent.Length > 0)
+            {
+                this.requestMessage.Content = new ByteArrayContent(messageContent);
+
+                // Apply cached "Content" header values
+                foreach (var contentHeader in this.contentHeaderValueCache)
+                {
+                    this.requestMessage.Content.Headers.Add(contentHeader.Key, contentHeader.Value);
+                }
+            }
+
+            this.requestMessage.Method = new HttpMethod(this.ActualMethod);
+
+            var send = this.client.SendAsync(this.requestMessage);
+            return send;
+        }
+
+        private static IDictionary<string, string> HttpHeadersToStringDictionary(HttpHeaders headers)
+        {
+            return headers.ToDictionary((h1) => h1.Key, (h2) => string.Join(",", h2.Value));
+        }
+
+        private static HttpClientResponseMessage ConvertHttpClientResponse(HttpResponseMessage response)
+        {
+            return new HttpClientResponseMessage(response);
+        }
+    }
+
+    public static class TaskExtensionMethods
+    {
+        public static Task<TResult> ToApm<TResult>(this Task<TResult> task, AsyncCallback callback, object state)
+        {
+            var tcs = new TaskCompletionSource<TResult>(state);
+
+            task.ContinueWith(
+                delegate
+                {
+                    if (task.IsFaulted)
+                    {
+                        tcs.TrySetException(task.Exception.InnerExceptions);
+                    }
+                    else if (task.IsCanceled)
+                    {
+                        tcs.TrySetCanceled();
+                    }
+                    else
+                    {
+                        tcs.TrySetResult(task.Result);
+                    }
+
+                    if (callback != null)
+                    {
+                        callback(tcs.Task);
+                    }
+                },
+                CancellationToken.None,
+                TaskContinuationOptions.None,
+                TaskScheduler.Default);
+
+            return tcs.Task;
+        }
+    }
+
+    public class HttpClientResponseMessage : IODataResponseMessage, IDisposable
+    {
+        private readonly IDictionary<string, string> headers;
+        private readonly Func<Stream> getResponseStream;
+        private readonly int statusCode;
+
+#if DEBUG
+        /// <summary>set to true once the GetStream was called.</summary>
+        private bool streamReturned;
+#endif
+
+        public HttpClientResponseMessage(HttpResponseMessage httpResponse)
+        {
+            this.headers = HttpHeadersToStringDictionary(httpResponse.Headers);
+            this.statusCode = (int)httpResponse.StatusCode;
+            this.getResponseStream = () => { var task = httpResponse.Content.ReadAsStreamAsync(); task.Wait(); return task.Result; };
+        }
+
+        private static IDictionary<string, string> HttpHeadersToStringDictionary(HttpHeaders headers)
+        {
+            return headers.ToDictionary((h1) => h1.Key, (h2) => string.Join(",", h2.Value));
+        }
+
+        /// <summary>
+        /// Returns the collection of response headers.
+        /// </summary>
+        public virtual IEnumerable<KeyValuePair<string, string>> Headers
+        {
+            get { return this.headers; }
+        }
+
+        /// <summary>
+        /// The response status code.
+        /// </summary>
+        public virtual int StatusCode
+        {
+            get { return this.statusCode; }
+
+            set { throw new NotSupportedException(); }
+        }
+
+        public virtual string GetHeader(string headerName)
+        {
+            string result;
+            if (this.headers.TryGetValue(headerName, out result))
+            {
+                return result;
+            }
+
+            // Since the unintialized value of ContentLength header is -1, we need to return
+            // -1 if the content length header is not present
+            if (string.Equals(headerName, "Content-Length", StringComparison.Ordinal))
+            {
+                return "-1";
+            }
+
+            return null;
+        }
+
+        public virtual void SetHeader(string headerName, string headerValue)
+        {
+            if (String.IsNullOrEmpty(headerValue))
+            {
+                return;
+            }
+            if (this.headers.ContainsKey(headerName))
+            {
+                this.headers[headerName] = headerValue;
+            }
+            else
+            {
+                this.headers.Add(headerName, headerValue);
+            }
+        }
+
+        public virtual Stream GetStream()
+        {
+#if DEBUG
+            Debug.Assert(!this.streamReturned, "The GetStream can only be called once.");
+            this.streamReturned = true;
+#endif
+
+            return this.getResponseStream();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+
 }
--- CrmOdataClient/Program.cs
+++ CrmOdataClient/Program.cs
@@ -3,45 +3,102 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
+using System.Threading.Tasks;
 using System.Xml;
+using Microsoft.Dynamics.CRM;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using Microsoft.OData;
 using Microsoft.OData.Client;
 using Microsoft.OData.Edm;
 using Microsoft.OData.Edm.Csdl;
+using Newtonsoft.Json.Linq;
 
 namespace CrmOdataClient
 {
     class Program
     {
+        private static string _token;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _token = GetToken("365d020a-b2c1-412e-b9f8-b78c36c61501", "Blah", "https://login.microsoftonline.com/83f8ad7c-2f65-4302-9950-b0e6b2b72fbf/oauth2/v2.0/authorize").GetAwaiter().GetResult();
+            var context = new Microsoft.Dynamics.CRM.System(new Uri("https://{crmUrl}/"));
 
-            var interestedProxyTypes = new Hashtable();
-            interestedProxyTypes.Add("contact", "contact");
-            interestedProxyTypes.Add("account", "account");
+            context.SendingRequest2 += ContextOnSendingRequest2;
+
+            var contactId = Guid.Parse("99db51a2-c34e-e111-bb8d-00155d03a715");
+
+            var dataServiceCollection =
+                new DataServiceCollection<contact>(context.contacts.Where(x => x.contactid == contactId))
+                {
+                    [0] = {firstname = "Portal1"}
+                };
+            context.SaveChanges(SaveChangesOptions.PostOnlySetProperties);
+
+
+            //CleanupMetaDataFile();
+        }
+
+        private static async Task<string> GetToken(string clientId, string clientSecret, string url)
+        {
+            var authenticationContext = new AuthenticationContext("https://login.microsoftonline.com/83f8ad7c-2f65-4302-9950-b0e6b2b72fbf");
+            var result = await authenticationContext.AcquireTokenAsync("https://mytraining22.api.crm11.dynamics.com",
+                new ClientCredential(clientId, clientSecret));
+
+            return result.AccessToken;
+        }
+
+        private static void ContextOnSendingRequest2(object? sender, SendingRequest2EventArgs e)
+        {
+            e.RequestMessage.SetHeader("Authorization", $"Bearer {_token}");
+        }
+
+        private static DataServiceClientRequestMessage OnMessageCreating(DataServiceClientRequestMessageArgs args)
+        {
+            var message = new HttpClientRequestMessage(args.ActualMethod) { Url = args.RequestUri, Method = args.Method, };
+            foreach (var header in args.Headers)
+            {
+                message.SetHeader(header.Key, header.Value);
+            }
+
+            return message;
+        }
+
+        private static void CleanupMetaDataFile()
+        {
+            var interestedProxyTypes = new Hashtable { { "contact", "contact" }, { "account", "account" } };
 
             var fileStream = File.OpenRead("metadata.xml");
             var edmModel = CsdlReader.Parse(new XmlTextReader(fileStream));
 
             var customEdmModel = new CustomEdmModel(edmModel);
-            var filteredSchemaElements = new List<IEdmSchemaElement>();
+            var filteredSchemaElements = new Dictionary<string, IEdmSchemaElement>(StringComparer.OrdinalIgnoreCase);
             foreach (var edmModelSchemaElement in edmModel.SchemaElements)
             {
-                if (interestedProxyTypes.ContainsKey(edmModelSchemaElement.Name))
+                var modelSchemaElement = edmModelSchemaElement as IEdmEntityType;
+                if (modelSchemaElement != null)
                 {
-                    filteredSchemaElements.Add(edmModelSchemaElement);
+                    var filterEdmEntityModel = new FilterEdmEntityModel(modelSchemaElement, interestedProxyTypes);
+                    if (interestedProxyTypes.ContainsKey(edmModelSchemaElement.Name))
+                    {
+                        filteredSchemaElements.Add(edmModelSchemaElement.Name, filterEdmEntityModel);
+
+                        if (filterEdmEntityModel.BaseType != null)
+                        {
+                            var baseType = filterEdmEntityModel.BaseType as IEdmEntityType;
+                            if (!filteredSchemaElements.ContainsKey(baseType.Name))
+                            {
+                                filteredSchemaElements.Add(baseType.Name, baseType);
+                            }
+                        }
+                    }
                 }
             }
 
-            var filterdPropertySchemaElements =new List<IEdmSchemaElement>();
-            foreach (IEdmEntityType filteredSchemaElement in filteredSchemaElements)
-            {
-                filterdPropertySchemaElements.Add(new FilterEdmEntityModel(filteredSchemaElement, interestedProxyTypes));
-            }
-
-            customEdmModel.SchemaElements = filterdPropertySchemaElements.AsEnumerable();
+            customEdmModel.SchemaElements = filteredSchemaElements.Select(x => x.Value).AsEnumerable();
             customEdmModel.VocabularyAnnotations = edmModel.VocabularyAnnotations;
             customEdmModel.DeclaredNamespaces = edmModel.DeclaredNamespaces;
             customEdmModel.DirectValueAnnotationsManager = edmModel.DirectValueAnnotationsManager;
